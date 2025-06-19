3e9ea42582f6bb3fccd04ba67b9f0c1de9d0f7b7
kuke997 <ttnf918@gmail.com>
2025-06-19 23:55:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-import asyncio
+import threading
 import time
+import asyncio
 from clicker.click_logic import run_click_once
+import server
+
+# 启动 HTTP Server 保活
+threading.Thread(target=server.run_server, daemon=True).start()
+
+async def loop_click():
+    while True:
+        await run_click_once()
+        time.sleep(6)
 
 if __name__ == "__main__":
-    while True:
-        asyncio.run(run_click_once())
-        time.sleep(6)  # 每分钟10次，每次间隔6秒
-
+    asyncio.run(loop_click())
