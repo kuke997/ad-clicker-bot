7b93136df7cc26f0ca7b3776c8b476da50c13f3a
kuke997 <ttnf918@gmail.com>
2025-06-19 23:21:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+import asyncio
+import time
+from clicker.click_logic import run_click_once
+
+if __name__ == "__main__":
+    while True:
+        asyncio.run(run_click_once())
+        time.sleep(6)  # 每分钟10次，每次间隔6秒
 
