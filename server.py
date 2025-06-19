a0111b1f9d8cab1214c328608b5d3c690e0603ec
kuke997 <ttnf918@gmail.com>
2025-06-19 23:56:05 +0800

    Update server.py

--- server.py
+++ server.py
@@ -1,1 +1,11 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"OK")
+
+def run_server():
+    HTTPServer(('0.0.0.0', 8000), Handler).serve_forever()
 
